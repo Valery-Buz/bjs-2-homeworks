384aeee6404fe766c7d397ee201228fe209a898f
Valery Buz <Ajven@yandex.ru>
2022-11-09 22:48:39 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,27 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b * b - 4 * a * c;
+
+  if (d > 0) {
+    let x = (-b + Math.sqrt(d)) / (2 * a);
+    let y = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x, y);
+  } else if (d == 0) {
+    let z = -b / (2 * a);
+    arr.push(z);
+  } else {
+    arr = [];
+  }
+    
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+//function calculateTotalMortgage(percent, contribution, amount, date) {
+  //let totalAmount;
 
   // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+  //return totalAmount;
+//}
