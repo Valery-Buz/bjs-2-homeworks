2c923923f2686869f1f1df346f4b1461635c0e6a
Valery Buz <Ajven@yandex.ru>
2022-11-12 22:14:08 +0300

    Initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,60 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = - Infinity;
+  sum = 0;
 
-  // Ваш код
-
+  for (let number of arr) {
+    if (number < min) {
+      min = number
+    }
+    if (number > max) {
+      max = number;
+    }
+    sum += number;
+  }
+  avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  
+  for (let arg of arr) {
+    sum += arg;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
+  for (let arr of arrOfArr) {
+    let sum = func(arr);
+    if (max < sum) {
+      max = sum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let arg of arr) {
+    if (min > arg) {
+      min = arg;
+    }
+    if (max < arg) {
+      max = arg;
+    }
+
+  }
+  return Math.abs(min - max);
 }
